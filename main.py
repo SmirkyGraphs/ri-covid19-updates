6d539cb7958063ee7fcd5f9d470f2fbfb04ffc94
Alex <SmirkyGraphs@gmail.com>
2020-05-04 04:19:19 -0400

    add nursing homes + zip

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from src import tweeter
 
 raw_general = './data/raw/ri-covid-19.csv'
 raw_geo = './data/raw/geo-ri-covid-19.csv'
-raw_facility = './data/raw/facility-covid-19.csv'
-raw_zip = './data/raw/zip-codes-covid-19.csv'
 raw_dem = './data/raw/demographics-covid-19.csv'
+raw_zip = './data/raw/zip-codes-covid-19.csv'
+raw_nurse_homes = './data/raw/nurse-homes-covid-19.csv'
 
 main_sheet = '1n-zMS9Al94CPj_Tc3K7Adin-tN9x1RSjjx2UzJ4SV7Q/export?format=csv&gid='
 facility_sheet = '2PACX-1vQUem_oGj3-KdLreZ3swTBx6q264zWqJUWc39nWSgE-KSYoqDRmlFyksSIP7H_lLNTVtr5lHbudeDz0'
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     tweeter.send_tweet()
 
     # scrape, clean infrequently updated data
-    scraper.scrape_nursing_homes(facility_sheet, raw_facility)
+    scraper.scrape_nursing_homes(facility_sheet, raw_nurse_homes)
     scraper.scrape_zip_codes(main_sheet, raw_zip)
+
+    cleaner.clean_nursing_homes(raw_nurse_homes)
     cleaner.clean_zip_codes(raw_zip)
-    cleaner.clean_facilities(raw_facility)
