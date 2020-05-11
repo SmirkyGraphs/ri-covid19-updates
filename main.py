662fc1f2f2e3668e4bcad121fa69088b623021e5
Alex <SmirkyGraphs@gmail.com>
2020-05-10 20:51:05 -0400

    refactor file names

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from src import scraper
 from src import cleaner
 from src import tweeter
-
-raw_general = './data/raw/ri-covid-19.csv'
-raw_geo = './data/raw/geo-ri-covid-19.csv'
-raw_dem = './data/raw/demographics-covid-19.csv'
-raw_zip = './data/raw/zip-codes-covid-19.csv'
-raw_nurse_homes = './data/raw/nurse-homes-covid-19.csv'
+from src import plots
 
 main_sheet = '1n-zMS9Al94CPj_Tc3K7Adin-tN9x1RSjjx2UzJ4SV7Q/export?format=csv&gid='
 facility_sheet = '2PACX-1vQUem_oGj3-KdLreZ3swTBx6q264zWqJUWc39nWSgE-KSYoqDRmlFyksSIP7H_lLNTVtr5lHbudeDz0'
 
 if __name__ == "__main__":
     # scrape, clean & tweet out general information
-    scraper.scrape_sheet(main_sheet, raw_general, raw_geo, raw_dem)
-    cleaner.clean_general(raw_general)
-    cleaner.clean_geographic(raw_geo)
+    scraper.scrape_sheet(main_sheet)
+    cleaner.clean_general('ri-covid-19')
+    cleaner.clean_geographic('geo-ri-covid-19')
     tweeter.send_tweet()
 
     # scrape, clean infrequently updated data
-    scraper.scrape_nursing_homes(facility_sheet, raw_nurse_homes)
-    scraper.scrape_zip_codes(main_sheet, raw_zip)
+    scraper.scrape_nursing_homes(facility_sheet)
+    scraper.scrape_zip_codes(main_sheet)
+    cleaner.clean_nursing_homes('nurse-homes-covid-19')
+    cleaner.clean_zip_codes('zip-codes-covid-19')
 
-    cleaner.clean_nursing_homes(raw_nurse_homes)
-    cleaner.clean_zip_codes(raw_zip)
+    # scrape revised data
+    scraper.scrape_revised(main_sheet)
+    cleaner.clean_revised('revised-data')
+
+    # make graphs
+    plots.testing_plot()
+    plots.hospital_plot()
+    plots.city_rate_plot()
