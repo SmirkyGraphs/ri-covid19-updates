293df05d0856fc31db7373e402d987b6ace9d073
Alex <SmirkyGraphs@gmail.com>
2020-05-10 20:45:28 -0400

    Create plots.py

---
+++ src/plots.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+plt.style.use('seaborn')
+plt.style.use('fivethirtyeight')
+date_format = mdates.DateFormatter('%#m/%#d')
+
+footer = 'made by: SmirkyGraphs  |  site: ivizri.com  |  source: RIDOH'
+
+def testing_plot():
+    print('\n[status] creating testing plots')
+    df = pd.read_csv('./data/clean/revised-data-clean.csv', parse_dates=['Date'])
+    df = df[-14:] # last 14 days
+
+    min_date = df['Date'].min().strftime("%#m/%d/%y")
+    max_date = df['Date'].max().strftime("%#m/%d/%y")
+    date_range = f'between {min_date} - {max_date}'
+
+    def fit_trend(y):
+        temp_df = df[df[y].notnull()].copy()
+        z = np.polyfit(temp_df['Date_ts'], temp_df[y], 1)
+        p = np.poly1d(z)
+        
+        return p(df['Date_ts'])
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True)
+    fig.suptitle("14-Day Trend of Testing", fontsize=16)
+
+    # plot data
+    axs[0,0].plot(df['Date'], df['New positive labs'], c='dodgerblue', linewidth=2)
+    axs[0,1].plot(df['Date'], df['New negative labs'], c='dodgerblue', linewidth=2)
+    axs[1,0].plot(df['Date'], df['New total labs'], c='dodgerblue', linewidth=2)
+    axs[1,1].plot(df['Date'], df['%_positive'], c='dodgerblue', linewidth=2)
+
+    # add trendline
+    axs[0,0].plot(df['Date'], fit_trend('New positive labs'), c='coral', linewidth=2, linestyle='--')
+    axs[0,1].plot(df['Date'], fit_trend('New negative labs'), c='coral', linewidth=2, linestyle='--')
+    axs[1,0].plot(df['Date'], fit_trend('New total labs'), c='coral', linewidth=2, linestyle='--')
+    axs[1,1].plot(df['Date'], fit_trend('%_positive'), c='coral', linewidth=2, linestyle='--')
+
+    axs[0,0].set_title('new positive labs')
+    axs[0,1].set_title('new negative labs')
+    axs[1,0].set_title('new total labs')
+    axs[1,1].set_title('% new labs - positive')
+
+    axs[1,0].xaxis.set_major_formatter(date_format)
+    axs[1,1].xaxis.set_major_formatter(date_format)
+
+    fig.tight_layout(rect=[0, 0.05, 1, 0.90])
+    fig.set_size_inches(9, 7, forward=True)
+    fig.text(x=.5, y=0.92, s=date_range, fontsize=10, ha='center')
+    fig.text(x=0.97, y=0.03, s=footer, fontsize=10, ha='right')
+
+    plt.savefig('./figures/testing_14_day_trend.png', dpi=150)
+
+def hospital_plot():
+    print('[status] creating hospital plots')
+    df = pd.read_csv('./data/clean/revised-data-clean.csv', parse_dates=['Date'])
+    df = df[-14:] # last 14 days
+
+    min_date = df['Date'].min().strftime("%#m/%d/%y")
+    max_date = df['Date'].max().strftime("%#m/%d/%y")
+    date_range = f'between {min_date} - {max_date}'
+
+    def fit_trend(y):
+        temp_df = df[df[y].notnull()].copy()
+        z = np.polyfit(temp_df['Date_ts'], temp_df[y], 1)
+        p = np.poly1d(z)
+        
+        return p(df['Date_ts'])
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True)
+    fig.suptitle("14-Day Trend of Testing", fontsize=16)
+
+    # plot data
+    axs[0,0].plot(df['Date'], df['New hospital admissions'], c='dodgerblue', linewidth=2)
+    axs[0,1].plot(df['Date'], df['New hospital discharges'], c='dodgerblue', linewidth=2)
+    axs[1,0].plot(df['Date'], df['Hospital Deaths'], c='dodgerblue', linewidth=2)
+    axs[1,1].plot(df['Date'], df['Deaths'], c='dodgerblue', linewidth=2)
+
+    # add trendline
+    axs[0,0].plot(df['Date'], fit_trend('New hospital admissions'), c='coral', linewidth=2, linestyle='--')
+    axs[0,1].plot(df['Date'], fit_trend('New hospital discharges'), c='coral', linewidth=2, linestyle='--')
+    axs[1,0].plot(df['Date'], fit_trend('Hospital Deaths'), c='coral', linewidth=2, linestyle='--')
+    axs[1,1].plot(df['Date'], fit_trend('Deaths'), c='coral', linewidth=2, linestyle='--')
+
+    axs[0,0].set_title('hospital admissions')
+    axs[0,1].set_title('hospital discharges')
+    axs[1,0].set_title('hospital deaths')
+    axs[1,1].set_title('all deaths')
+
+    axs[1,0].xaxis.set_major_formatter(date_format)
+    axs[1,1].xaxis.set_major_formatter(date_format)
+
+    fig.tight_layout(rect=[0, 0.05, 1, 0.90])
+    fig.set_size_inches(9, 7, forward=True)
+    fig.text(x=.5, y=0.92, s=date_range, fontsize=10, ha='center')
+    fig.text(x=0.97, y=0.03, s=footer, fontsize=10, ha='right')
+
+    plt.savefig('./figures/hospital_14_day_trend.png', dpi=150)
+
+def city_rate_plot():
+    df = pd.read_csv('./data/clean/geo-ri-covid-19-clean.csv', parse_dates=['date'])
+    df = df.sort_values(by=['city_town', 'date'])
+
+    g = sns.FacetGrid(df, col="city_town", col_wrap=5, height=5)
+    g = g.map(plt.plot, "date", "rate_per_10k", marker=",")
+
+    xformatter = mdates.DateFormatter("%m/%d")
+    g.axes[0].xaxis.set_major_formatter(xformatter)
+    g.savefig("./figures/cities_rate.png", dpi=150)
