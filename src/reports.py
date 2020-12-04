f42cc16b563b6cca3f87bd2eef72283954e51f0e
Alex <SmirkyGraphs@gmail.com>
2020-12-03 23:49:10 -0500

    Update reports.py
    adds first deaths, full deaths, and hospital capacity

--- src/reports.py
+++ src/reports.py
@@ -1,26 +1,62 @@
 import pandas as pd
 
-def death_change(df, scraped, idx, report_name):
-    # get most recent 2 dates
-    prior_date = df.loc[df['date_scraped'] == scraped[idx]]
-    current = df.loc[df['date_scraped'] == scraped[-1]]
+def full_death_change(df):
+    df = df[['date_scraped', 'date', 'deaths']]
 
-    # set date as index and select only deaths
-    prior_date = prior_date.set_index('date')['deaths']
-    current = current.set_index('date')['deaths']
+    min_date = df['date'].min()
+    max_date = df['date'].max()
+    index = pd.date_range(min_date, max_date)
+    multi_index = pd.MultiIndex.from_product([df['date_scraped'].unique(), index])
 
-    if report_name == 'daily':
-        prior_date = prior_date.reindex_like(current).fillna(0)
+    df = df.set_index(['date_scraped', 'date'])
+    df = df.reindex(multi_index).fillna(0)
 
-    # get change between 2 dates
-    df = (current - prior_date).reset_index()
-    change = df[df['deaths'] != 0].dropna(axis=0)
-    change.to_csv(f'./data/reports/{report_name}_death_change.csv', index=False)
+    chng = df.groupby(level=1).diff()
+    chng = chng[chng['deaths'] != 0].dropna().reset_index()
+    chng.columns = ['date_scraped', 'date', 'deaths']
+    chng.to_csv('./data/reports/full_death_change.csv', index=False)
+
+def first_death_change(df):
+    df = df[['date_scraped', 'date', 'deaths']]
+
+    # get first recorded value
+    fst = df.drop_duplicates(subset=['date'], keep='first')
+    fst = fst.rename(columns={'deaths': 'first_reported_deaths', 'date_scraped': 'first_date_scraped'})
+
+    # get current value
+    df = df[df['date_scraped'] == df['date_scraped'].max()]
+    df = df.rename(columns={'deaths': 'current_deaths'})
+
+    # merge, get difference, order columns, save
+    df = df.merge(fst, on='date')
+    df['diff'] = df['current_deaths'] - df['first_reported_deaths']
+    df = df[['date', 'first_date_scraped', 'first_reported_deaths', 'date_scraped', 'current_deaths', 'diff']]
+    df.to_csv('./data/reports/first_death_change.csv', index=False)
+
+def hosp_capacity(df):
+    print('[status] getting hospital capacity % covid')
+
+    # get hospital total icu/clincal beds
+    cap = pd.read_csv('./data/external/hosp-capacity.csv')
+    cap = cap.rename(columns={'ICU Total Beds': 'icu_beds', 'Clinical Total Beds': 'clinical_beds'})
+    cap = cap[['icu_beds', 'clinical_beds']].sum().reset_index()
+    cap.columns = ['metric', 'capacity']
+
+    # get current hospital stats
+    df = df[['currently hospitalized', 'icu']].dropna(axis=0, how='all')[-1:]
+    df = df.rename(columns={'currently hospitalized': 'clinical_beds', 'icu': 'icu_beds'})
+    df = df.squeeze().reset_index()
+    df.columns = ['metric', 'current']
+
+    # combine & save
+    df = df.merge(cap, how='left', on='metric')
+    df['%_capacity_covid'] = df['current']/df['capacity']
+    df.to_csv('./data/reports/hospital_capacity.csv', index=False)
 
 def run_reports():
-    df = pd.read_csv('./data/clean/revised-data-clean.csv', parse_dates=['date'])
-    scraped = df['date_scraped'].unique().tolist()
+    df = pd.read_csv('./data/clean/revised-data-clean.csv', parse_dates=['date_scraped', 'date'])
     
     # change in deaths
-    death_change(df, scraped, -2, 'daily')
-    death_change(df, scraped, 0, 'start')
+    full_death_change(df)
+    first_death_change(df)
+    hosp_capacity(df)
