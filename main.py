59be60d0339a662c1da3b0faa138f55fc0cd22b5
Alex <SmirkyGraphs@gmail.com>
2021-01-06 06:09:47 -0500

    1/5/2021 update

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ main_sheet = '1c2QrNMz8pIbYEKzMJL7Uh2dtThOJa2j1sSMwiDo5Gz4/export?format=csv&gid
 
 if __name__ == "__main__":
     # scrape, clean & tweet out general information
-    #scraper.scrape_sheet(main_sheet)
+    scraper.scrape_sheet(main_sheet)
     cleaner.clean_general('ri-covid-19')
     cleaner.clean_geographic('geo-ri-covid-19')
-    #tweeter.send_tweet()
+    tweeter.send_tweet()
 
     # scrape, clean infrequently updated data
-    #scraper.scrape_nursing_homes(main_sheet)
-    #scraper.scrape_zip_codes(main_sheet)
+    scraper.scrape_nursing_homes(main_sheet)
+    scraper.scrape_zip_codes(main_sheet)
     cleaner.clean_nursing_homes('nurse-homes-covid-19')
     cleaner.clean_zip_codes('zip-codes-covid-19')
 
     # scrape revised data
-    #scraper.scrape_revised(main_sheet)
+    scraper.scrape_revised(main_sheet)
     cleaner.clean_revised('revised-data')
 
     # make graphs
